89b49ad38b0207b9067be682022408ca6dbca8a2
David Stosik <david.stosik+git-noreply@gmail.com>
2022-12-03 09:06:20 +0900

    Download input from adventofcode.com

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ This repository is a Ruby project containing my answers to the [Advent of Code](
 
 - `bin/today` runs the solution for today's puzzle.
 - `rake` runs the test suite.
+- Pass your Advent of Code session cookie using the `AOC_SESSION` environment variable so that the program can automatically download input data.
--- lib/advent_of_code/input.rb
+++ lib/advent_of_code/input.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require "net/http"
+require "uri"
+
 module AdventOfCode
   class Input
     def initialize(year, day)
@@ -11,7 +14,8 @@ module AdventOfCode
       if File.exist?(local_path)
         File.read(local_path)
       else
-        raise "Input data was not found locally"
+        puts "Input for #{year}, day #{day} not found locally."
+        download
       end
     end
 
@@ -20,7 +24,35 @@ module AdventOfCode
     attr_reader :year, :day
 
     def local_path
-      File.join(__dir__, "..", "..", "data", year, "day#{day}.txt")
+      File.join(directory, "day#{day}.txt")
+    end
+
+    def directory
+      File.join(__dir__, "..", "..", "cache", year)
+    end
+
+    def download
+      raise "Unable to download input without session cookie." unless session_cookie
+
+      ensure_directory
+      puts "Downloading input for #{year}, day #{day}..."
+      uri = URI("https://adventofcode.com/#{year}/day/#{day}/input")
+      req = Net::HTTP::Get.new(uri)
+      req["Cookie"] = "session=#{session_cookie}"
+      res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+        http.request(req)
+      end
+      # FIXME handle errors
+      File.write(local_path, res.body)
+      res.body
+    end
+
+    def ensure_directory
+      Dir.mkdir(directory) unless Dir.exist?(directory)
+    end
+
+    def session_cookie
+      ENV["AOC_SESSION"]
     end
   end
 end
