4eb1504e272d212c9fd8872184a627c6ca700fa7
David Stosik <david.stosik+git-noreply@gmail.com>
2022-12-10 23:31:19 +0900

    Implement a way to run another day's puzzle

--- lib/advent_of_code/cli/today.rb
+++ lib/advent_of_code/cli/today.rb
@@ -11,8 +11,25 @@ module AdventOfCode
         parent.register(self, namespace, usage, desc)
       end
 
+      TIME_OFFSET = "-0500"
+      private_constant :TIME_OFFSET
+
+      argument :day,
+        type: :numeric,
+        optional: true,
+        default: Time.now.localtime(TIME_OFFSET).day,
+        desc: "The day on the Advent Calendar (1-25)",
+        banner: "DAY"
+
+      def check_existence
+        day_class
+      rescue NameError
+        say_error "No solution for #{date} yet.", :red
+        exit
+      end
+
       def command
-        say "Solving puzzles for #{time.strftime("%Y-%m-%d")}"
+        say "Solving puzzles for #{date}:"
         run_part(1)
         run_part(2)
       end
@@ -20,9 +37,11 @@ module AdventOfCode
       private
 
       def run_part(part)
-        return unless day_object.respond_to?("part#{part}")
-
-        say "Part #{part}: " + day_object.public_send("part#{part}").to_s
+        if day_object.respond_to?("part#{part}")
+          say "Part #{part}: " + day_object.public_send("part#{part}").to_s
+        else
+          say "Part #{part} not implemented yet", :yellow
+        end
       end
 
       def day_object
@@ -30,19 +49,20 @@ module AdventOfCode
       end
 
       def day_class
-        year_module.const_get("Day#{time.day}")
+        year_module.const_get("Day#{day}")
       end
 
       def year_module
-        AdventOfCode.const_get("Year#{time.year}")
+        AdventOfCode.const_get("Year#{year}")
       end
 
-      def time
-        @_time ||= Time.now.localtime(TIME_OFFSET)
+      def date
+        Date.new(year, 12, day)
       end
 
-      TIME_OFFSET = "-0500"
-      private_constant :TIME_OFFSET
+      def year
+        2022
+      end
     end
   end
 end
