8cdfde6f46f04d42f47e5b045cc1c8bf2d6d45da
David Stosik <david.stosik+git-noreply@gmail.com>
2023-01-06 09:55:34 +0900

    Implement a command to download problems in an HTML file, so that I can solve them in the plane

--- lib/advent_of_code/cli/app.rb
+++ lib/advent_of_code/cli/app.rb
@@ -36,6 +36,52 @@ module AdventOfCode
 
       desc "download [YEAR]", "Download all available inputs for a given year (defaults to the latest)"
       def download(year = nil)
+        year, latest_day = year_day(year)
+
+        say "Downloading inputs for #{year}..."
+
+        1.upto(latest_day) do |day|
+          Input.new(year, day).read
+        end
+
+        say "Done!"
+      end
+
+      desc "problems [YEAR]", "Download all available problems for a given year"
+      def problems(year = nil)
+        year, latest_day = year_day(year)
+
+        say "Downloading problems for #{year}..."
+
+        root = File.join(__dir__, "..", "..", "..")
+        problems_dir = File.join(root, "problems", year.to_s)
+        FileUtils.mkdir_p(problems_dir)
+
+        session_cookie = File.open(File.join(root, "cache", "session"), &:readline).strip
+
+        1.upto(latest_day) do |day|
+          # FIXME could we avoid redownloading? (problem: the second part might be available later)
+          say "Downloading problem for #{year}, day #{day}..."
+
+          # FIXME factorize "logged-in downloader"
+          uri = URI("https://adventofcode.com/#{year}/day/#{day}")
+          req = Net::HTTP::Get.new(uri)
+          req["Cookie"] = "session=#{session_cookie}"
+          req["User-Agent"] = "github.com/davidstosik/advent_of_code" # FIXME make configurable?
+          res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+            http.request(req)
+          end
+          # FIXME handle errors (eg 404)
+          local_path = File.join(problems_dir, "#{day}.html")
+          File.write(local_path, res.body)
+        end
+
+        say "Done!"
+      end
+
+      private
+
+      def year_day(year)
         latest_year = Time.now.year
         latest_year -= 1 unless Time.now.month == 12
 
@@ -52,13 +98,7 @@ module AdventOfCode
           latest_day = 25
         end
 
-        say "Downloading inputs for #{year}..."
-
-        1.upto(latest_day) do |day|
-          Input.new(year, day).read
-        end
-
-        say "Done!"
+        [year, latest_day]
       end
     end
   end
